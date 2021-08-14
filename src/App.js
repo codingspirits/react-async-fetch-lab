de88b120c61cea89aaeaab59d345a587ad34ce80
vincci-cheng-project-development-page <vincci.cheng.github@gmail.com>
2021-08-14 19:10:33 +1000

    Completed Assignment

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React, {Component} from 'react'
+
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            peopleInSpace : []}
+    }
+    render() {
+        return(
+        <div>{this.state.peopleInSpace.map(person => person.name)}</div>
+        )
+    }
+/*      componentDidMount = () => {
+        fetch ('http://api.open-notify.org/astros.json')
+        .then(response=>response.json())
+        .then(data=> {this.setState(
+            {peopleInSpace : data.people}
+        )})}  */
+
+        componentDidMount = async () => {
+            const response = await fetch('http://api.open-notify.org/astros.json')
+            const data = await response.json()
+        
+            this.setState(
+                {peopleInSpace : data.people})
+          }
+
+            
+        
+
+}
